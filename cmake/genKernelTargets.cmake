2a4bf2e713eee89841ee7db31f53e4972fabcb25
Arjun Ramaswami <arjunr@mail.upb.de>
2020-06-03 18:55:51 +0200

    cmake: synthesis not working

--- cmake/genKernelTargets.cmake
+++ cmake/genKernelTargets.cmake
@@ -13,8 +13,9 @@ function(gen_fft_targets)
   foreach(kernel_fname ${ARGN})
 
     set(CL_SRC "${CL_PATH}/${kernel_fname}.cl")
-    set(CL_INCLUDES "-I${CMAKE_BINARY_DIR}/kernels/common/fft_config.h")
     set(CL_INCL_DIR "-I${CMAKE_BINARY_DIR}/kernels/common")
+    set(CL_INCLUDES "-I${CMAKE_BINARY_DIR}/kernels/common/fft_config.h"
+    set(CL_HEADER "${CMAKE_BINARY_DIR}/kernels/common/fft_config.h")
 
     set(EMU_BSTREAM 
         "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${FFT_SIZE}_${kernel_fname}_emu.aocx")
@@ -52,17 +53,15 @@ function(gen_fft_targets)
 
     # Synthesis Target
     add_custom_command(OUTPUT ${SYN_BSTREAM}
-      COMMAND ${IntelFPGAOpenCL_AOC} ${CL_SRC} ${CL_INCL_DIR} ${AOC_FLAGS}  -board=${FPGA_BOARD_NAME} -o ${SYN_BSTREAM}
+      COMMAND ${IntelFPGAOpenCL_AOC} ${CL_SRC} ${CL_INCLUDES} ${AOC_FLAGS} -board=${FPGA_BOARD_NAME}  -o ${SYN_BSTREAM}
       MAIN_DEPENDENCY ${CL_SRC}
-      VERBATIM
     )
     
     add_custom_target(${kernel_fname}_syn
-      DEPENDS ${SYN_BSTREAM} "${CMAKE_BINARY_DIR}/kernels/common/fft_config.h"
+      DEPENDS ${SYN_BSTREAM} ${CL_SRC} ${CL_HEADER}
       COMMENT 
         "Building a report for ${kernel_fname} to folder ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}"
     )
-
-  endforeach(kernel_fname)
+  endforeach()
 
 endfunction()
--- scripts/fft3d_ddr.sh
+++ scripts/fft3d_ddr.sh
@@ -9,6 +9,7 @@ module load intelFPGA_pro/20.1.0 nalla_pcie/19.4.0_hpc
 module load numlib/FFTW
 
 cd ../build
-cmake -DLOG_FFT_SIZE=4 ..
+
+cmake -DLOG_FFT_SIZE=5 ..
 make
 make fft3d_ddr_triv_syn
