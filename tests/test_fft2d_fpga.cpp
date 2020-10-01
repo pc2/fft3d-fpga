047d23bd86bd57ec086feb151f382203e108785b
Arjun Ramaswami <arjunr@mail.upb.de>
2020-10-01 13:41:04 +0200

    Test: fix path to 2d bram

--- tests/test_fft2d_fpga.cpp
+++ tests/test_fft2d_fpga.cpp
@@ -47,7 +47,7 @@ TEST(fft2dFPGATest, CorrectnessBRAM){
   const int N = (1 << 6);
   fpga_t fft_time = {0.0, 0.0, 0.0, 0};
 
-  int isInit = fpga_initialize("Intel(R) FPGA", "emu_64_fft2d_bram/fft2d_bram.aocx", false);
+  int isInit = fpga_initialize("Intel(R) FPGA", "emu_64_fft2d_bram_opt/fft2d_bram_opt.aocx", false);
   EXPECT_EQ(isInit, 0);
 
   size_t sz = sizeof(float2) * N * N;
